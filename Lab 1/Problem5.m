86ffdc5d0662a0a5f8de60c0bbf7bc2a36b38927
sohailf7 <33504467+sohailf7@users.noreply.github.com>
2019-09-20 11:12:32 -0400

    Problem 5 (wrong for some reason)

--- Lab 1/Problem5.m
+++ Lab 1/Problem5.m
@@ -1,22 +1,24 @@
 %*************************************************************
 % Fowad Sohail
 % Lab1: Continuous-time Systems
-% Problem 2: Generate Ramp function
+% Problem 5
 % Inputs: time support and sampling steps of the signal
 % Output: unit step response
 % Reference: Signals and Systems with Matlab, Chaparro Luis F.
 %**************************************************************
 
 clear all;
-close all;
-clf
-Ts = 0.01; % Sampling time
-t = -5:Ts:5; % support of signal
-% unit-step function with support [-5,5], delayed by 3
-y = ramp(t, 3, 0);
-plot(t,y);
-axis([-5 5 -1 5]);
-title('Problem 2');
-xlabel('time (seconds)');
-ylabel('y(t)');
+clf;
+t = -10:0.01:10;
+y1 = ramp(t,2,2.5);
+y2 = ramp(t,-5,0);
+y3 = ramp(t,3,-2);
+y4 = ustep(t,-4);
+y = y1 + y2 + y3 + y4;
+plot(t,y,'k');
+axis([-10 10 -3 5]);
 grid
+
+
+% a.) y = 2r(t+2.5) - 5r(t) + 3r(t-2) + u(t-4)
+% b.) 
