e72c4fa07291eb04f007c736cc40a2170eec3963
sohailf7 <33504467+sohailf7@users.noreply.github.com>
2019-09-27 11:13:33 -0400

    Problem 5b and documentation

--- Lab 1/ramp.m
+++ Lab 1/ramp.m
@@ -13,9 +13,11 @@ function y = ramp(t,m, ad)
 
 N= length(t);
 y = zeros(1,N);
+
 for i = 1:N
     if t(i) >= -ad
-    y(i) = m*(t(i) + ad);
+%         y(i) = m*(t(i))*ustep(t(i), ad);
+        y(i) = m*(t(i) + ad);
     end
 end
 end
