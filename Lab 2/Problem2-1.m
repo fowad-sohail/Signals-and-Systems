3de2ba34d2b6d49c735bcd8163d8832bb04bd458
sohailf7 <33504467+sohailf7@users.noreply.github.com>
2019-10-11 10:54:02 -0400

    Problem 2 code and figure

---
+++ Lab 2/Problem2-1.m
@@ -0,0 +1,16 @@
+%*************************************************************
+% Fowad Sohail
+% Lab 2: Chirps Problem 1
+% Reference: Signals and Systems with Matlab, Chaparro Luis F.
+%**************************************************************
+
+t = 0:0.05:40;
+x = 1 * cos(2*t+(t.^2)/4);
+
+figure(4);
+plot(t,x)
+title('Signal Generation Problem 3');
+xlabel('Time Domain');
+ylabel('Amplitude');
+axis([0 40 -2 2])
+grid
